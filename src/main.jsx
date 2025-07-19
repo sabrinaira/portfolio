b0c1fb837b8616f32eb81bb5f14957b473acc1e7
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-07-18 21:47:25 -0400

    Style: - Migrated variables to globals.scss, - Migrated about and projects stylings into their own .scss files, - Updated media queries on About, Projects, and Contact components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import './styles/styles.scss';
 
-if ('scrollRestoration' in window.history) {
-  window.history.scrollRestoration = 'manual';
-}
+// if ('scrollRestoration' in window.history) {
+//   window.history.scrollRestoration = 'manual';
+// }
 
 const root = createRoot(document.getElementById('app'));
 
